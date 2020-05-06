5725a879861bab57383eed99ccd7b95cda5d55e2
gnapiorkowski <gustaw.napiorkowski@gmail.com>
2020-05-06 14:02:05 +0200

    added random number generation

--- docker-my-frontend/src/App.js
+++ docker-my-frontend/src/App.js
@@ -1,23 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import RandomNumber from './RandomNumber.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-	  Hello, this is a feature! Noice job!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <RandomNumber />
       </header>
     </div>
   );
